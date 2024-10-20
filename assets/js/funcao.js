0e3a030bf3358744e14d5a1cde1096b5a82abe08
annaagabi <annaa0478@gmail.com>
2024-10-20 19:26:23 -0300

    Feat: atualização da modal para inserir e armazenar informações localmente

--- assets/js/funcao.js
+++ assets/js/funcao.js
@@ -10,27 +10,70 @@ function Enviar() {
 
 // Modal
 
-const openModalButton = document.querySelector("#open-modal")
-const modal = document.querySelector("#modal")
+// const openModalButton = document.querySelector("#open-modal")
+// const modal = document.querySelector("#modal")
 
-const toggleModal = () =>{
-    modal.classList.toggle("hide-modal")
+// const toggleModal = () =>{
+//     modal.classList.toggle("hide-modal")
+// }
+
+// // Abrir o modal
+// openModalButton.addEventListener("click", () => {
+//     toggleModal();
+  
+//     // Fecha a modal após 5 segundos
+//     setTimeout(() => {
+//       toggleModal();
+//     }, 5000); 
+//   });
+  
+
+// Modal
+
+const openModalButton = document.querySelector("#open-modal");
+const modal = document.querySelector("#modal");
+
+const toggleModal = () => {
+    modal.classList.toggle("hide-modal");
 }
 
 // Abrir o modal
+// openModalButton.addEventListener("click", () => {
+//     toggleModal();
+  
+//     // Fecha o modal após 5 segundos e redireciona para "novanoticia.html"
+//     setTimeout(() => {
+//         toggleModal();
+        
+//         // Redireciona para a nova página
+//         window.location.href = "todasNoticias.html";
+//     }, 5000); 
+// });
+
+// Abrir o modal
 openModalButton.addEventListener("click", () => {
-    toggleModal();
+  const tituloValue = document.getElementById("noticia-titulo").value;
+  const descricaoValue = document.getElementById("noticia-descricao").value;
   
-    // Fecha a modal após 5 segundos
-    setTimeout(() => {
+  if (tituloValue && descricaoValue) {
+      // Salvar dados no localStorage
+      localStorage.setItem("tituloNoticia", tituloValue);
+      localStorage.setItem("descricaoNoticia", descricaoValue);
+      
+      // Exibe o modal
       toggleModal();
-    }, 5000); 
-  });
-  
+    
+      // Fecha o modal após 5 segundos e redireciona para "todasNoticias.html"
+      setTimeout(() => {
+          toggleModal();
+          window.location.href = "todasNoticias.html";
+      }, 5000); 
+  }
+});
+
+
 // Redimensionar Textarea
-
-
-const textarea = document.getElementById('area-redimensionar');
+const textarea = document.getElementById('noticia-descricao');
 
 textarea.addEventListener('input', function() {
   this.style.height = 'auto';
