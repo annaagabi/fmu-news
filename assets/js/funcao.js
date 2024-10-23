9ba89d3d0c8c9cfe107b40ac38fe2b8e0d84d202
annaagabi <annaa0478@gmail.com>
2024-10-23 11:37:11 -0300

    Fix: removendo função

--- assets/js/funcao.js
+++ assets/js/funcao.js
@@ -1,13 +1,3 @@
-function Enviar() {
-
-    var nome = document.getElementById("email");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
-}
-  
 // Modal para todas as noticias
 
 const openModalButton = document.querySelector("#open-modal")
