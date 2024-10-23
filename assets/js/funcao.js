3064ee0a58d40d5479a157679d71941bc9c751af
annaagabi <annaa0478@gmail.com>
2024-10-23 12:54:40 -0300

    Feat: adicionado função para span de campos vazios

--- assets/js/funcao.js
+++ assets/js/funcao.js
@@ -8,34 +8,68 @@ const toggleModal = () =>{
 // Abrir o modal
 if (openModalButton) {
     openModalButton.addEventListener("click", () => {
-        toggleModal();
+        toggleModal()
       
         // Fecha a modal após 3 segundos
         setTimeout(() => {
-          toggleModal();
-          window.location.href = "../../pages/todas-noticias.html";
-        }, 3000);
+          toggleModal()
+          window.location.href = "../../pages/todas-noticias.html"
+        }, 3000)
     });
 }
 
 // Modal para deletar a notícia 
-const deleteModalButton = document.querySelector("#delete-modal");
+const deleteModalButton = document.querySelector("#delete-modal")
 if (deleteModalButton) {
     deleteModalButton.addEventListener("click", () => {
-        toggleModal();
+        toggleModal()
       
         setTimeout(() => {
-          toggleModal();
-        }, 3000);
+          toggleModal()
+        }, 3000)
+    });
+}
+const noticiaModalButton = document.querySelector("#noticia-modal");
+
+
+if (noticiaModalButton) {
+    noticiaModalButton.addEventListener("click", () => {
+        const titulo = document.querySelector("#noticia-titulo").value.trim();
+        const descricao = document.querySelector("#noticia-descricao").value.trim();
+
+        const spanTitulo = document.querySelector("#span-texto");
+        const spanDescricao = document.querySelector("#span-descricao");
+
+        spanTitulo.classList.add("hide-modal");
+        spanDescricao.classList.add("hide-modal");
+
+        // Verifica se os campos estão preenchidos
+        if (titulo !== "" && descricao !== "") {
+            document.querySelector("#modal .modal-texto p").textContent = "Post editado com sucesso!";
+            toggleModal();
+
+            setTimeout(() => {
+                window.location.href = "../../pages/todas-noticias.html"
+                toggleModal();
+            }, 3000);
+        } else {
+            // Exibe spans de erro se os campos estiverem vazios
+            if (titulo === "") {
+                spanTitulo.classList.remove("hide-modal");
+            }
+            if (descricao === "") {
+                spanDescricao.classList.remove("hide-modal");
+            }
+        }
     });
 }
 
 // Redimensionar Textarea
-const textarea = document.getElementById('noticia-descricao');
+const textarea = document.getElementById('noticia-descricao')
 
 textarea.addEventListener('input', function() {
-  this.style.height = 'auto';
+  this.style.height = 'auto'
   this.style.height = (this.scrollHeight)   
- + 'px';
-});
+ + 'px'
+})
   
