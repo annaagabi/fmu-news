0d3d55bde9506ca5ce0b5a2fa1c38c9de3aa106d
annaagabi <annaa0478@gmail.com>
2024-11-18 12:16:54 -0300

    Feat: adicionando validação de campos

--- assets/js/funcao.js
+++ assets/js/funcao.js
@@ -94,6 +94,74 @@ if (loginButton) {
     });
 }
 
+
+const cadastroButton = document.querySelector("#cadastro");
+if (cadastroButton) {
+    cadastroButton.addEventListener("click", (event) => {
+        event.preventDefault(); 
+
+        const nome = document.querySelector("#nome").value.trim();
+        const email = document.querySelector("#email").value.trim();
+        const senha = document.querySelector("#senha").value.trim();
+        const confSenha = document.querySelector("#conf-senha").value.trim();
+
+        const spanNome = document.querySelector("#span-nome");
+        const spanEmail = document.querySelector("#span-email");
+        const spanSenha = document.querySelector("#span-senha");
+        const spanConfSenha = document.querySelector("#span-conf-senha");
+
+        // Reseta os spans de erro
+        spanNome.classList.add("hide-modal");
+        spanEmail.classList.add("hide-modal");
+        spanSenha.classList.add("hide-modal");
+        spanConfSenha.classList.add("hide-modal");
+
+        let isValid = true;
+
+        // Validação dos campos
+        if (nome === "") {
+            spanNome.classList.remove("hide-modal");
+            isValid = false;
+        }
+        // Validação de email usando RegEx
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+        if (!emailRegex.test(email)) {
+            spanEmail.textContent = "Insira um email válido!";
+            spanEmail.classList.remove("hide-modal");
+            isValid = false;
+        }
+
+        // Validação de senha usando RegEx
+        const senhaRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        if (!senhaRegex.test(senha)) {
+            spanSenha.textContent = 
+                "A senha deve ter no mínimo 8 caracteres, incluindo uma letra maiúscula, uma minúscula, um número e um caractere especial!";
+            spanSenha.classList.remove("hide-modal");
+            spanSenha.classList.add("span-erro-longo");
+            isValid = false;
+        }
+        if (confSenha === "") {
+            spanConfSenha.classList.remove("hide-modal");
+            isValid = false;
+        }
+        if (senha !== confSenha) {
+            spanConfSenha.textContent = "As senhas não coincidem!";
+            spanConfSenha.classList.remove("hide-modal");
+            isValid = false;
+        }
+
+        // Se todos os campos forem válidos, exibe o modal e redireciona
+        if (isValid) {
+            toggleModal();
+            setTimeout(() => {
+                toggleModal();
+                window.location.href = "./todas-noticias.html";
+            }, 3000);
+        }
+    });
+}
+
+
 // Redimensionar Textarea
 const textarea = document.getElementById('noticia-descricao')
 
