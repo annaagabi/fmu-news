cef26b1ef5d49648b084b91fcf7d2eb2733ef572
annaagabi <annaa0478@gmail.com>
2024-11-25 18:24:41 -0300

    Fix: Trocando a extensão de todas as noticias para php e adicionando a lógica para aparecer o id do usuario e apara conectar ao banco

--- todas-noticias.php
+++ todas-noticias.php
@@ -1,3 +1,14 @@
+<?php
+    include_once "conexao.php";
+    date_default_timezone_set('America/Sao_Paulo');
+
+    try {
+        $select = $con->prepare("SELECT * FROM post");
+        $select->execute();
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -5,51 +16,55 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todas as Noticias</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/todas-style.css">
+    <link rel="stylesheet" href="./assets/css/todas-style.css">
 </head>
 <body>
     <header class="borda-vermelha border-2 border-top-0 border-end-0 border-start-0 mb-5">
         <div class="d-flex justify-content-between align-items-center m-5 mb-4 cabecalho">
-            <img src="../assets/imagens/logo.svg" alt="Lodo do FMU News com o FMU na cor vermelha e o News na cor azul">
+            <img src="./assets/imagens/logo.svg" alt="Lodo do FMU News com o FMU na cor vermelha e o News na cor azul">
             <a href="./index.html"><button class="botao-estilo botao-cadastro borda-raio fw-bold fonte-24">SAIR</button></a>
         </div>
     </header>
     <main class="container d-flex justify-content-center align-items-center flex-column teste">
         <div class="d-flex largura justify-content-center gap-botoes botoes-todas">
-            <a href="./nova-noticia.html"><button class="botao-estilo botao-cadastro borda-raio fw-bold fonte-24 botao-post">NOVO POST</button></a>
+            <a href="./nova-noticia.php"><button class="botao-estilo botao-cadastro borda-raio fw-bold fonte-24 botao-post">NOVO POST</button></a>
             <a href="./cadastro.html"><button class="botao-estilo botao-usuario borda-raio fw-bold fonte-24 botao-usuario mr-5">NOVO USUARIO</button></a>
+        </div>
+        <div class="hide-modal">
+            <input type="text" name="usuario" id="usuario" class="form-control"
+                           value= <?php session_start(); echo $_SESSION['id'];?> readonly> <!-- Mostra o id do usuario -->
         </div>
         <!-- <div id="cards-noticia" class="cards-noticia d-flex align-items-center justify-content-center flex-column mt-5">
             <div class="card-noticia"></div>
         </div> -->
       <!-- <div id="cards-noticia"> -->
-      <div id="cards-noticia" class="cards-noticia d-flex align-items-center justify-content-center flex-column mt-5 position-relative">
+      <div id="cards-noticia" class="cards-noticia d-flex align-items-center justify-content-center flex-column mt-3 position-relative">
         <div class="card-noticia d-flex align-items-center justify-content-center gap-3 mt-5">
             <div class="titulo d-flex align-items-center justify-content-center rounded-4 fundo-azul h4-largura">
                 <h4 class="texto-branco fw-bold fonte-24">Titulo</h4>
             </div>
             <div class="funcoes d-flex gap-1">
-                <a href="editar.html" class="edita-btn"><img src="../assets/imagens/editar.svg" alt="Icone de edição representado por um lápis e um papel na cor vermelha"></a>
-                <img id="delete-modal" src="../assets/imagens/lixeira.svg" alt="Icone de deletar representado por uma lixeira na cor vermelha">
+                <a href="editar.html" class="edita-btn"><img src="./assets/imagens/editar.svg" alt="Icone de edição representado por um lápis e um papel na cor vermelha"></a>
+                <img id="delete-modal" src="./assets/imagens/lixeira.svg" alt="Icone de deletar representado por uma lixeira na cor vermelha">
             </div>
         </div>
        </div>
 
        <div id="modal" class="modal-container d-flex align-items-center justify-content-center position-fixed hide-modal">
         <div class="modal-texto d-flex align-items-center justify-content-center fonte-20 texto-preto">
-          <img src="../assets/imagens/modal-check.svg" alt="">
+          <img src="./assets/imagens/modal-check.svg" alt="">
           <p>Post deletado com sucesso!</p>
         </div>
       </div>
     </main>
     <footer class="fundo-vermelho mt-5 d-flex justify-content-between align-items-center w-100 position-relative bottom-0">
         <p class="texto-branco fonte-16 ms-5">2024 FMU NEWS. All Rights Reserved By Anna Gabriela, Charles, Gabriel & Gabriella</p>
-        <a href="https://github.com/annaagabi/fmu-news" target="_blank" class="me-5"><img src="../assets/imagens/github.svg" alt="Logo do github na cor branca"></a>
+        <a href="https://github.com/annaagabi/fmu-news" target="_blank" class="me-5"><img src="./assets/imagens/github.svg" alt="Logo do github na cor branca"></a>
     </footer>
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
-    <script src="../assets/js/funcao.js"></script>
+    <script src="./assets/js/funcao.js"></script>
 </body>
 </html>
